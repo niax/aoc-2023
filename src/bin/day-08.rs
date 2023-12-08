57a141c7ab74364539532a389f9cc0735be91d3d
Nicholas Hollett <niax@niax.co.uk>
2023-12-07 23:35:58 -0800

    Do stupid bit things.

--- src/bin/day-08.rs
+++ src/bin/day-08.rs
@@ -1,25 +1,22 @@
 use aoc_2023::commons::io::Input;
 use aoc_2023::commons::math::LcmExt;
-use petgraph::graph::{DiGraph, NodeIndex};
-use petgraph::visit::EdgeRef;
-use std::collections::HashMap;
 use std::error::Error;
 
 #[inline]
-fn path_length<F: Fn(NodeIndex) -> bool>(
-    graph: &DiGraph<&str, char>,
+fn path_length<F: Fn(u32) -> bool>(
+    nodes: &[(u32, u32)],
     path: &str,
-    start_node: NodeIndex,
+    start_node: u32,
     end: F,
 ) -> usize {
     let mut current_node = start_node;
     for (i, dir) in path.chars().cycle().enumerate() {
-        for edge_ref in graph.edges(current_node) {
-            if *edge_ref.weight() == dir {
-                current_node = edge_ref.target();
-                break;
-            }
-        }
+        let options = nodes[current_node as usize];
+        current_node = match dir {
+            'L' => options.0,
+            'R' => options.1,
+            _ => panic!("AHHH!"),
+        };
 
         if end(current_node) {
             return i + 1;
@@ -29,6 +26,14 @@ fn path_length<F: Fn(NodeIndex) -> bool>(
     usize::MAX
 }
 
+#[inline]
+fn node_as_int(s: &str) -> u32 {
+    let mut bytes = s.bytes();
+    ((bytes.next().unwrap() - b'A') as u32) << 10
+        | ((bytes.next().unwrap() - b'A') as u32) << 5
+        | ((bytes.next().unwrap() - b'A') as u32)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = Input::from_argv()?;
     let mut lines = input.as_str().lines();
@@ -36,40 +41,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = lines.next().unwrap();
     lines.next();
 
-    let mut nodes = HashMap::with_capacity(1000);
-    let mut ending_in_a = Vec::with_capacity(1000);
-    let mut graph = DiGraph::<&str, char>::with_capacity(1000, 1000);
+    let mut nodes = [(u32::MAX, u32::MAX); 32768];
+    let mut ending_in_a = Vec::with_capacity(1_000);
     for l in lines {
         let node_name = &l[0..3];
-        let left_path = &l[7..10];
-        let right_path = &l[12..15];
+        let node = node_as_int(node_name);
+        let left_path = node_as_int(&l[7..10]);
+        let right_path = node_as_int(&l[12..15]);
 
-        let node_idx = *nodes
-            .entry(node_name)
-            .or_insert_with(|| graph.add_node(node_name));
-        let left_node = *nodes
-            .entry(left_path)
-            .or_insert_with(|| graph.add_node(left_path));
-        let right_node = *nodes
-            .entry(right_path)
-            .or_insert_with(|| graph.add_node(right_path));
-
-        graph.add_edge(node_idx, left_node, 'L');
-        graph.add_edge(node_idx, right_node, 'R');
+        nodes[node as usize] = (left_path, right_path);
         if node_name.ends_with('A') {
-            ending_in_a.push(node_idx);
+            ending_in_a.push(node);
         }
     }
 
-    let start_node = *nodes.get("AAA").unwrap();
-    let target_node = *nodes.get("ZZZ").unwrap();
-    let part1 = path_length(&graph, path, start_node, |node| node == target_node);
+    let target_node = node_as_int("ZZZ");
+    let part1 = path_length(&nodes, path, 0, |node| node == target_node);
 
-    let part2 = ending_in_a.iter().map(|node_idx| {
-        path_length(&graph, path, *node_idx, |node| {
-            graph.node_weight(node).unwrap().ends_with('Z')
-        })
-    }).lcm().unwrap();
+    let part2 = ending_in_a
+        .iter()
+        .map(|node_idx| path_length(&nodes, path, *node_idx, |node| node & 0b11111 == 0b11001))
+        .lcm()
+        .unwrap();
 
     println!("{}\n{}", part1, part2);
 
